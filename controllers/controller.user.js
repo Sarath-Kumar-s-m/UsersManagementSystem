c6b4ae1960c64a0d626abf6b9f717bbb5dda9ebf
Sarath-Kumar-s-m <sarath.20102000@gmail.com>
2025-01-12 13:26:09 +0530

    style(controllers/controller.user.js): format the file

--- controllers/controller.user.js
+++ controllers/controller.user.js
@@ -1,47 +1,40 @@
-
-
-
 const home = async (req, res) => {
-     
-    if(req.session.userData){
-   
-        res.status(200).render('userHome',{
+
+    if (req.session.userData) {
+        res.status(200).render("userHome", {
             data: req.session.userData,
             error: undefined,
-            message: "Successfully Logged In !" 
-        })
-    
-    }else{
-      
-        res.status(404).render('userHome', {
+            message: "Successfully Logged In !",
+        });
+    } else {
+        res.status(404).render("userHome", {
             data: undefined,
             error: "Can't Fetch User Information !",
-            message: undefined
-        }); 
-    
+            message: undefined,
+        });
     }
-}
 
+}
 
 const signOut = async (req, res) => {
-      
-    if(req.session.role == 'user'){
-           req.session.destroy(function(error){
-              if(error){
+
+    if (req.session.role == "user") {
+        req.session.destroy(function (error) {
+            if (error) {
                 throw error;
-              }else{
-                res.status(200).render("loginForm",{data:{
-                    error: undefined
-                }
-                })
-              }
-           })
-      }
+            } else {
+                res.status(200).render("loginForm", {
+                    data: {
+                        error: undefined,
+                    },
+                });
+            }
+        });
+    }
 
 }
-
 
 module.exports = {
     home,
-    signOut
-}
+    signOut,
+};
