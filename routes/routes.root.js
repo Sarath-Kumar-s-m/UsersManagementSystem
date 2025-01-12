eb336fa75bbc326f6efd5aae77e57d62f94fbdab
Sarath-Kumar-s-m <sarath.20102000@gmail.com>
2025-01-12 14:23:26 +0530

    style(routes/routes.root.js): format the file

--- routes/routes.root.js
+++ routes/routes.root.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const rootControllers = require('../controllers/controller.root.js');
 
-
 const rootRoutes = express.Router();
 
 
 rootRoutes.get('/', rootControllers);
-
 
 module.exports = rootRoutes;
