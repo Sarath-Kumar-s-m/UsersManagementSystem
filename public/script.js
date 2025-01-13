18d29501721f1abcbc0770eea251220ac2697402
Sarath-Kumar-s-m <sarath.20102000@gmail.com>
2025-01-13 22:36:49 +0530

    delete(public/script.js): delete example search function

--- public/script.js
+++ public/script.js
@@ -1,28 +1,12 @@
- 
-  
- 
- function toggle(){
-     let element = document.getElementById('showPassword');
-     let input = document.getElementById('password');
-  
-     if(element.checked){
-           input.type = 'text'
-         }else{
-           input.type = 'password'
-         }       
-  }
+function toggle() {
+    let element = document.getElementById("showPassword");
+    let input = document.getElementById("password");
+
+    if (element.checked) {
+        input.type = "text";
+    } else {
+      input.type = "password";
+    }
+}
 
 
-  async function search(){
-       let value = document.getElementById('input-1').value;
-       if(value == ""){
-         window.alert('Enter somthing in the input box !') 
-       }else{
-           try{
-            const response = await axios.get('http://localhost:3000/admin/dashboard/api/v1/search');
-            console.log(response)        
-           }catch(error){
-             console.log(error)
-           }
-       }
- }
